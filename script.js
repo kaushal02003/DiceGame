afc38ecf34e50b97588f99f367d4004994c34b17
kaushal <kaushalpandey14022000@gmail.com>
2025-07-19 20:47:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+var randomno1 = Math.floor(Math.random() * 6) + 1;
+var randomimg1 = "images/" + "dice" + randomno1 + ".png";
+var randomdiceimg1 = document.querySelectorAll("img")[0];
+randomdiceimg1.setAttribute("src", randomimg1);
+
+var randomno2 = Math.floor(Math.random() * 6) + 1;
+var randomimg2 = "images/" + "dice" + randomno2 + ".png";
+var randomdiceimg2 = document.querySelectorAll("img")[1];
+randomdiceimg2.setAttribute("src", randomimg2);
+
+if(randomno1 > randomno2){
+    document.querySelector("h1").innerHTML="🚩 Player 1 Wins";
+}
+else if(randomno2 > randomno1){
+    document.querySelector("h1").innerHTML="🚩 Player 2 Wins";
+}
+else{
+    document.querySelector("h1").innerHTML="Draw!";
+}
